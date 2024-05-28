bc97ed633b8bed9a77179c633ca1506bc3e06456
Dhayal1234 <ankitdhayal00004@gmail.com>
2024-05-28 17:18:55 +0530

    ME_QMONEY_V2_MODULE_REST_API  (Crio.Do)

--- qmoney/src/test/java/com/crio/warmup/stock/PortfolioManagerApplicationTest.java
+++ qmoney/src/test/java/com/crio/warmup/stock/PortfolioManagerApplicationTest.java
@@ -21,8 +21,8 @@ class PortfolioManagerApplicationTest {
   @Test
   void mainReadFile() throws Exception {
     //given
-    String filename = "assessments/trades.json";
-    List<String> expected = Arrays.asList(new String[]{"MSFT", "CSCO", "CTS"});
+    String filename = "trades.json";
+    List<String> expected = Arrays.asList(new String[]{"AAPL", "MSFT", "GOOGL"});
 
     //when
     List<String> results = PortfolioManagerApplication
@@ -30,26 +30,21 @@ class PortfolioManagerApplicationTest {
 
     //then
     Assertions.assertEquals(expected, results);
-  
-
-   
   }
 
 
   @Test
   void mainReadQuotes() throws Exception {
-     //given
-     String filename = "assessments/trades.json";
-     List<String> expected = Arrays.asList(new String[]{"CTS", "CSCO", "MSFT"});
- 
-     //when
-     List<String> actual = PortfolioManagerApplication
-         .mainReadQuotes(new String[]{filename, "2019-12-12"});
- 
-     //then
-     Assertions.assertEquals(expected, actual);
+    //given
+    String filename = "trades.json";
+    List<String> expected = Arrays.asList(new String[]{"MSFT", "AAPL", "GOOGL"});
 
-  
+    //when
+    List<String> actual = PortfolioManagerApplication
+        .mainReadQuotes(new String[]{filename, "2019-12-12"});
+
+    //then
+    Assertions.assertEquals(expected, actual);
   }
 
 
